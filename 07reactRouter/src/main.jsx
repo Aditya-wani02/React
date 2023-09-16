9204f740f9735f54670ce711af2b664fdee613d9
Aditya <adityawani0215@gmail.com>
2023-09-16 10:50:07 +0530

    ReactRouter

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -9,6 +9,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { gitHubInfoLoader } from './components/Github/Github.jsx'
 
 
 // const router = createBrowserRouter([
@@ -46,6 +47,12 @@ createRoutesFromElements(
   <Route path='about' element= {<About/>} />
   <Route path='contact' element= {<Contact/>} />
   <Route path='user/:userid' element= {<User/>} />
+  <Route 
+  loader={gitHubInfoLoader}
+  path='github'
+   element= {<Github/>}
+    />
+
 
 
 </Route>
