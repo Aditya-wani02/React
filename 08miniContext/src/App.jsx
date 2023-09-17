10ff5662443ba92f0a83291fe0d643682185bb6e
Aditya <adityawani0215@gmail.com>
2023-09-17 23:57:52 +0530

    Login/Profile

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
@@ -8,6 +10,10 @@ function App() {
     <UserContextProvider>
 
      <h1>React Learn </h1>
+      <Login/>
+      <Profile/>
+
+
     </UserContextProvider>
   )
 }
