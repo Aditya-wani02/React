a868b9af3d0c5441238c6f0d489562249434f422
Aditya <adityawani0215@gmail.com>
2023-11-03 10:58:15 +0530

    Pages

--- 12BlogSite/src/components/AuthLayout.jsx
+++ 12BlogSite/src/components/AuthLayout.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { UseSelector, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function Protected({ children, authentication = true }) {
   const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
-  const authService = useSelector((state) => state.auth.status);
+  const authStatus  = useSelector((state) => state.auth.status);
   useEffect(() => {
     // TODO: make it more easy
     if (authentication && authStatus !== authentication) {
